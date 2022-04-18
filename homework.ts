b860d0ceebc5ce015b99118c25a622f27e4bf876
Bogaciov Daniil <71877453+BDaniil@users.noreply.github.com>
2022-04-18 18:26:05 +0300

    Updated Decorators

--- homework.ts
+++ homework.ts
@@ -1,10 +1,10 @@
 // EX 1 -----------------------------------
 
 interface Entity {
-    readonly id?: string;
-    readonly name?: string;
-    readonly age?: number;
-    readonly ethnicity?: string;
+  readonly id?: string;
+  readonly name?: string;
+  readonly age?: number;
+  readonly ethnicity?: string;
 }
 
 // Given following interface do the following operations:
@@ -15,22 +15,22 @@ interface Entity {
 // expected:
 // type newType = A<B<C<D<Type>>>>
 /*
-    {
-      name: boolean;  
-      age: boolean;
-    }
-   */
+  {
+    name: boolean;  
+    age: boolean;
+  }
+ */
 
 // 1. Remove readonly
 
 type A<T> = {
-    -readonly [P in keyof T]: T[P];
+  -readonly [P in keyof T]: T[P];
 };
 
 // 2. Remove optional type
 
 type B<T> = {
-    [P in keyof T]-?: T[P];
+  [P in keyof T]-?: T[P];
 };
 
 // 3. Remove id property & ethnicity property
@@ -40,44 +40,42 @@ type C<T> = Omit<T, "id" | "ethnicity">;
 // 4. Mapper function that maps response type of all keys to boolean
 
 type D<T> = {
-    [P in keyof T]: boolean;
+  [P in keyof T]: boolean;
 };
 
 type newType = A<B<C<D<Entity>>>>;
 
 // EX 2 ------------------------------------------------
 /*
-  /*HW
-  1. Create an interface Id that has property id of type number
-  2. Create an interface Name that has property name of type string
-  3. Create a new type IdOrName and pass in a generic type
-  * If passed in type extends Id, IdOrName - will be of type number
-  * Else If passed in type extends Name, IdOrName - will be of type string
-  * Else passed in type extends Anything Else, IdOrName - will be of type {age: boolean}
-  */
+/*HW
+1. Create an interface Id that has property id of type number
+2. Create an interface Name that has property name of type string
+3. Create a new type IdOrName and pass in a generic type
+* If passed in type extends Id, IdOrName - will be of type number
+* Else If passed in type extends Name, IdOrName - will be of type string
+* Else passed in type extends Anything Else, IdOrName - will be of type {age: boolean}
+*/
 
 interface Id {
-    id: number;
+  id: number;
 }
 
 interface Name {
-    name: string;
+  name: string;
 }
-interface check {
-    check: string;
-}
-type IdOrName<T> = T extends Id
-    ? number
-    : T extends Name
-    ? string
-    : { age: boolean };
 
-type newTypeSecond = IdOrName<Id>;
+type IdOrName<T> = T extends Id
+  ? number
+  : T extends Name
+  ? string
+  : { age: boolean };
+
+type newTypeSecond = IdOrName<Name>;
 
 // EX 3 ------------------------------------------------
 /*
-  Write a detailed explanation with images  steps  words how ex 5 withLet function works and why did we get the expected result
-  */
+Write a detailed explanation with images  steps  words how ex 5 withLet function works and why did we get the expected result
+*/
 
 // var app = () => {
 
@@ -179,14 +177,14 @@ type newTypeSecond = IdOrName<Id>;
 //Having two interfaces:
 
 interface User {
-    id: number;
-    name: string;
-    age: number;
+  id: number;
+  name: string;
+  age: number;
 }
 interface Car {
-    id: number;
-    color: string;
-    numberOfDoors: number;
+  id: number;
+  color: string;
+  numberOfDoors: number;
 }
 
 // Replicate an API response that will have the following structure:
@@ -199,37 +197,37 @@ interface Car {
 // }
 
 type ApiResponse<T> = {
-    data: {
-        [key: string]: T | number;
-        pagination: number;
-    };
-    errors: string[];
+  data: {
+    [key: string]: T | number;
+    pagination: number;
+  };
+  errors: string[];
 };
 
 //-------------Checks
 
 let objUser: ApiResponse<User> = {
-    data: {
-        user: {
-            id: 23,
-            age: 23,
-            name: "Max",
-        },
-        pagination: 23,
+  data: {
+    user: {
+      id: 23,
+      age: 23,
+      name: "Max",
     },
-    errors: [],
+    pagination: 23,
+  },
+  errors: [],
 };
 
 let objCar: ApiResponse<Car> = {
-    data: {
-        user: {
-            id: 23,
-            color: "black",
-            numberOfDoors: 4,
-        },
-        pagination: 23,
+  data: {
+    user: {
+      id: 23,
+      color: "black",
+      numberOfDoors: 4,
     },
-    errors: [],
+    pagination: 23,
+  },
+  errors: ["yes", "no"],
 };
 
 //------------------------------------
@@ -237,77 +235,112 @@ let objCar: ApiResponse<Car> = {
 // EX 5 ------------------------------
 // Write a class decorator, method decorator and parameter decorator functions for any Class the logic inside each decorator is up to you e.g.:
 /*
-  @ClassDecorator
-  class SomeClass {
-    @PropertyDecorator
-    property1: string;
-    @MethodDecorator(PASS_SOME_ENUM)
-    someMethod(@ParameterDecorator someParameter: number) {
-      // If PASS_SOME_ENUM value === 0 => Print parameter decorator value + some text from @ParameterDecorator
-      // Else Print parameter decorator value * 50 + some text from @ParameterDecorator
-      console.log('this is our message');
-    }
+@ClassDecorator
+class SomeClass {
+  @PropertyDecorator
+  property1: string;
+  @MethodDecorator(PASS_SOME_ENUM)
+  someMethod(@ParameterDecorator someParameter: number) {
+    // If PASS_SOME_ENUM value === 0 => Print parameter decorator value + some text from @ParameterDecorator
+    // Else Print parameter decorator value * 50 + some text from @ParameterDecorator
+    console.log('this is our message');
   }
-   */
+}
+ */
+
+enum PassedEnum {
+  first,
+  second,
+  third,
+  fourth,
+}
 
 function ClassDecorator(target: Function) {
-    console.log("I'm Class decorator!", "My target is " + target);
+  //  console.log("I'm Class decorator!", "My target is " + target)
+  target.prototype.created = new Date().toLocaleString("es-ES");
+  console.log("Assignment checked : " + target.prototype.created);
 }
 
-function MethodDecorator(enumerable: number) {
-    return function (
-        target: Object,
-        propertyName: string,
-        propertyDescriptor: PropertyDescriptor
-    ) {
-        console.log(
-            "I'm Method Decorator!",
-            "My target is " + target,
-            "My Property is " + propertyName,
-            "My propertyDescriptor is " + propertyDescriptor
-        );
-        propertyDescriptor.value = function (value: number) {
-            if (enumerable === 0) {
-                return value;
-            } else {
-                return value * 50;
-            }
-        };
+function MethodDecorator(EnumExample: {
+  first: number;
+  second: number;
+  third: number;
+  fourth: number;
+}) {
+  return function (
+    _target: Object,
+    propertyName: string,
+    propertyDescriptor: PropertyDescriptor
+  ) {
+    //    console.log("I'm Method Decorator!","My target is " + target,
+    //    "My Property is " + propertyName, "My propertyDescriptor is " + propertyDescriptor)
+    const originalMethod = propertyDescriptor.value;
+
+    propertyDescriptor.value = function (...args: any[]) {
+      const result = originalMethod.apply(this, args);
+
+      Object.keys(EnumExample)
+        .filter(key => !isNaN(Number(key)))
+        .forEach(key => {
+          if (Number(key) === 0) {
+            console.log(
+              ParameterDecorator.call(this, this, propertyName, result)
+            );
+          } else {
+            console.log(
+              ParameterDecorator.call(this, this, propertyName, result * 50)
+            );
+          }
+        });
     };
+  };
 }
 
-function PropertyDecorator(target: Object, propertyName: string) {
-    console.log(
-        "I'm Property Decorator!",
-        "My target is " + target,
-        "My Property is " + propertyName
-    );
+function PropertyDecorator(property: number | string) {
+  //  console.log("I'm Property Decorator!", "My target is " + target, "My Property is " + propertyName)
+  return function (target: Object, propertyName: string): void {
+    const getter = () => {
+      return property;
+    };
+    const setter = () => {
+      console.log(`Setting value to ${property}`);
+      return property;
+    };
+
+    Object.defineProperty(target, propertyName, {
+      get: getter,
+      set: setter,
+    });
+  };
 }
 
 function ParameterDecorator(
-    target: number,
-    propertyName: string,
-    index: number
-) {
-    console.log(
-        "I'm Parameter Decorator!",
-        "My target is " + target,
-        "My Property is " + propertyName,
-        "My index is " + index
-    );
+  _target: Object,
+  _propertyName: string,
+  index: number
+): any {
+  //  console.log("I'm Parameter Decorator!","My target is " + target, "My Property is " + propertyName, "My index is " + index);
+  return index + " is the result";
 }
 
 @ClassDecorator
 class TestClass {
-    @PropertyDecorator
-    id: number | undefined;
+  @PropertyDecorator(22)
+  id: number = 20;
 
-    @MethodDecorator(10)
-    returnValue(@ParameterDecorator someValue: number) {
-        return someValue;
-    }
+  @PropertyDecorator("Daniil")
+  name: string = "Danik";
+
+  @MethodDecorator(PassedEnum)
+  returnValue(@ParameterDecorator someValue: number) {
+    console.log("this is our message");
+    return someValue;
+  }
 }
 
 const ts = new TestClass();
 
-console.log(ts.returnValue(7));
+ts.returnValue(7);
+
+console.log(ts.id);
+console.log(ts.name);
